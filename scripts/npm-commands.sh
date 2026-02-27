2629835059c7bfee42b68c02dd076b6f1382a7f8
interaction <243127395@qq.com>
2026-02-27 11:10:38 +0800

    mv build scripts to bash shell

--- scripts/npm-commands.sh
+++ scripts/npm-commands.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-build:api() {
-  mkdir -p dist/_worker.js
-  npx esbuild api/index.ts --bundle --minify --format=esm --outfile=dist/_worker.js/index.js \
-    --external:*.png --external:*.svg --external:*.jpg --external:*.jpeg \
-    --external:*.gif --external:*.woff --external:*.woff2 --external:*.ttf --external:*.eot
+build() {
+  rm -rf dist
+  npm run build:fe && npm run build:blog
 }
 
 "$@"
